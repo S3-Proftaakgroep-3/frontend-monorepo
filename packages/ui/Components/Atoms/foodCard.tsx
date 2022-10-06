708edd19386bd03d3aaa24e2e4f5d4299d15c14a
Steijn Ploegmakers <461991@student.fontys.nl>
2022-10-06 13:48:41 +0200

    feature: simple card made

--- packages/ui/Components/Atoms/foodCard.tsx
+++ packages/ui/Components/Atoms/foodCard.tsx
@@ -1,38 +1,34 @@
 import * as React from "react";
 import styles from '../../Styles/Atoms/foodCard.module.css'
+import fonts from '../../Styles/Utils/fonts.module.css'
+import classNames from "classnames";
 
 export const FoodCard = (props: any) => {
     return <div className={styles.page}>
         <div className="row">
             <div className={styles.column}>
-                <div className={styles.card}>
-                    <div className="container">
-                        <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
-                        <h2>Walkout</h2>
-                        <p>Lekkere Pizza</p>
-                    </div>
+                <div className={styles.container}>
+                    <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
+                    <div className={classNames(styles.title, fonts.xl_card)}>Pizza Pepperoni</div><br/>
+                    <div className={classNames(styles.description, fonts.m_card)}>Zalige pizza met mozzarella en dungesneden plakjes Pepperoni</div>
                 </div>
             </div>
         </div>
         <div className="row">
             <div className={styles.column}>
-                <div className={styles.card}>
-                    <div className="container">
-                        <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
-                        <h2>Walkout</h2>
-                        <p>Lekkere Pizza</p>
-                    </div>
+                <div className={styles.container}>
+                    <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
+                    <div className={classNames(styles.title, fonts.xl_card)}>Pizza Pepperoni</div><br/>
+                    <div className={classNames(styles.description, fonts.m_card)}>Zalige pizza met mozzarella en dungesneden plakjes Pepperoni</div>
                 </div>
             </div>
         </div>
         <div className="row">
             <div className={styles.column}>
-                <div className={styles.card}>
-                    <div className="container">
-                        <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
-                        <h2>Walkout</h2>
-                        <p>Lekkere Pizza</p>
-                    </div>
+                <div className={styles.container}>
+                    <img className={styles.image} src="https://images0.persgroep.net/rcs/5XA03cGALr-SKZ0BlzVGmD6vMo8/diocontent/114943802/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt=""/>
+                    <div className={classNames(styles.title, fonts.xl_card)}>Pizza Pepperoni</div><br/>
+                    <div className={classNames(styles.description, fonts.m_card)}>Zalige pizza met mozzarella en dungesneden plakjes Pepperoni</div>
                 </div>
             </div>
         </div>
---
+++ packages/ui/Components/Atoms/verticalFoodCard.tsx
@@ -0,0 +1,8 @@
+import styles from "../../Styles/Atoms/verticalFoodCard.module.css";
+import * as React from "react";
+
+export const VerticalFoodCard = (props: any) => {
+    return <div>
+
+    </div>
+}
