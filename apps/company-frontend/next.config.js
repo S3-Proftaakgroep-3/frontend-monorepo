d4593d0a19b82d5bff23c5e1e1849fee8ed37a9b
492563@student.fontys.nl <Vribherfee5>
2022-10-13 14:03:40 +0200

    removed experimental

--- apps/company-frontend/next.config.js
+++ apps/company-frontend/next.config.js
@@ -2,6 +2,5 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  experimental: { nftTracing: true },
 });
 
--- apps/customers-frontend/next.config.js
+++ apps/customers-frontend/next.config.js
@@ -2,5 +2,4 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  experimental: { nftTracing: true },
 });
