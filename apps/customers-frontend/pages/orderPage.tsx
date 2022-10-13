83728d0669fa080a735b185c13dd94766b0da71d
Steijn Ploegmakers <461991@student.fontys.nl>
2022-10-13 15:17:05 +0200

    orderpage is made

--- apps/customers-frontend/pages/orderPage.tsx
+++ apps/customers-frontend/pages/orderPage.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import {NextPage} from "next";
 import styles from "../styles/Home.module.css";
 import {NewOrderMenu} from "ui/Components/Molecules/newOrderMenu";
+import {OrderCard} from "ui";
+
 const orderPage : NextPage = () => {
     return(
-        <div>
-
-            <div className={styles.container}>
-                <NewOrderMenu price={19.99}/>
+        <div className={styles.page}>
+            <OrderCard title={"Pizza Pepperoni"} price={19.99} quantity={5}/>
+            <div className={styles.orderMenu}>
+                <NewOrderMenu price={99.95}/>
             </div>
         </div>
     )
