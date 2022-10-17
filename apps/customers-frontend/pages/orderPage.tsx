3897d0367eb7816fc79acf13f03a8630ad983232
MikeVerlinde <mike.verlinde@outlook.com>
2022-10-17 09:53:36 +0200

    top menu secondary

--- apps/customers-frontend/pages/orderPage.tsx
+++ apps/customers-frontend/pages/orderPage.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import {NextPage} from "next";
 import styles from "../styles/Home.module.css";
 import {NewOrderMenu} from "ui/Components/Molecules/newOrderMenu";
+import { TopMenuSecondary } from "ui";
 const orderPage : NextPage = () => {
     return(
         <div>
-
             <div className={styles.container}>
                 <NewOrderMenu price={19.99}/>
             </div>
