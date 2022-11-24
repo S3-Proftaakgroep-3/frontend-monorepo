0ea3ee025d1afc11d03b6f7163a832afdcb1abb2
Steijn Ploegmakers <461991@student.fontys.nl>
2022-11-24 09:27:47 +0100

    started with api call for orders

--- apps/company-frontend/pages/index.tsx
+++ apps/company-frontend/pages/index.tsx
@@ -1,21 +1,50 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { CategoryBtn } from 'ui'
+import {IRestaurant, OrderCard,} from 'ui'
 import * as React from "react";
+import {ICartItem} from "ui/Interfaces/ICartItem";
+import {useEffect, useState} from "react";
+
+interface ContextTypes {
+    query: any
+}
+
+export async function getServerSideProps({ query }: ContextTypes) {
+
+    // Query
+    const restaurantId = query.restaurant
+
+    // Fetch
+    const res = await fetch(`https://mdma-order-service.herokuapp.com/api/order/${restaurantId}/all`)
+    const data = await res.json()
+
+    return {
+        props: {
+            orders: data as ICartItem
+        }
+    }
+}
 
 const Home: NextPage = () => {
+
+    const [order, setOrder] = useState<any[]>([])
+
+    const getOrder = async() => {
+        const orders = await fetch('https://mdma-order-service.herokuapp.com/api/order/all')
+        const data = await orders.json()
+        setOrder(data)
+        console.log(order)
+    }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Company</title>
-        <meta name="description" content="This is the backend for restaurants, they can modify their application here." />
-        <link rel="icon" href="burger.ico" />
-      </Head>
-        <CategoryBtn label='test' active={true} onClick={() => {}}/>
-    </div>
-  )
+        <div id={styles.page}>
+            {
+                order.map((orderItem: ICartItem, key: number) => {
+                    return <OrderCard key={key} item={orderItem}/>
+                })
+            }
+        </div>
+    )
 }
 
 export default Home
