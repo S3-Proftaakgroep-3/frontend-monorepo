0d8fd090274d8dec677347c67b17b79030570bfd
Reno Muijsenberg <490412@student.fontys.nl>
2022-12-02 10:36:50 +0100

    Get back old code

--- apps/company-frontend/pages/[restaurant]/dashboard/ready.tsx
+++ apps/company-frontend/pages/[restaurant]/dashboard/ready.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next"
-import styles from '../../styles/dashboard.module.css'
+import styles from '../../../styles/dashboard.module.css'
 import { NavBar, FullOrderCard } from "ui/Components"
 import {ICartItem} from "ui/Interfaces/ICartItem";
 import {useEffect, useState} from "react";
@@ -7,40 +7,38 @@ import {IRestaurant} from "ui";
 import {IOrder} from "ui/Interfaces/IOrder";
 import {SectionProductsPerOrder} from "ui/Components/Molecules/sectionProductsPerOrder";
 import * as React from "react";
+import {useRouter} from "next/router";
 
-export async function getServerSideProps({ query }: ContextTypes) {
 
-    // Query
-    const restaurantId = query.restaurant
 
-    // Fetch
-    const res = await fetch(`https://mdma-restaurant-service.herokuapp.com/api/restaurant/get?id=${restaurantId}`)
-    const data = await res.json()
-
-    return {
-        props: {
-            restaurant: data as IRestaurant
+const Ready: NextPage = () => {
+    const router = useRouter();
+    const [pathName, setPathName] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        if (router.isReady) {
+            
+            const path = Array.isArray(router.query.restaurant) ? router.query.restaurant[0] : router.query.restaurant
+            
+            if (path) {
+                setPathName(path)
+            }
+            
         }
-    }
-}
-
-interface PropTypes {
-    restaurant: IRestaurant
-}
-
-const Ready: NextPage<PropTypes> = ( {restaurant}: PropTypes ) => {
-
-    const urlEndpoint = `https://localhost:8080/api/order/subscribe/${restaurant.id}`;
-    let eventSource = null;
+    }, [router.isReady])
+    
+    
     const [orders, setOrders] = useState<any[]>([]);
 
     useEffect(() => {
-        eventSource = new EventSource(urlEndpoint);
+        if (pathName) {
+            const urlEndpoint = `http://localhost:8080/api/order/subscribe/${pathName}`;
+            let eventSource = new EventSource(urlEndpoint);
 
-        eventSource.addEventListener("Latest's Orders", (event) => {
-            setOrders(JSON.parse(event.data))
-        })
-    }, [])
+            eventSource.addEventListener("Latest's Orders", (event) => {
+                setOrders(JSON.parse(event.data))
+            })
+        }
+    }, [pathName])
 
     return (
         <>
