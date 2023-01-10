1380f1351a485f211026f477c915a8470e54dccc
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2023-01-10 11:19:22 +0100

    Save custom styles in localstorage on QR code scan

--- apps/customers-frontend/pages/index.tsx
+++ apps/customers-frontend/pages/index.tsx
@@ -2,26 +2,81 @@ import {NextPage} from "next";
 import * as React from "react";
 import {useRouter} from "next/router";
 import {useEffect} from "react";
-import {wait} from "next/dist/build/output/log";
+import styles from "../styles/fonts.module.css"
 
-const Index: NextPage = () => {
+interface ContextTypes {
+    query: any
+}
+
+export async function getServerSideProps({query}: ContextTypes) {
+    try {
+        const restaurantId = query.restaurantId
+        const tableId = query.tableId
+
+        const res = await fetch(`https://mdmawhitelabelservice.azurewebsites.net/api/whitelabel/get/${restaurantId}`)
+        const data = await res.json();
+
+        return {
+            props: {
+                restaurantId: restaurantId,
+                tableId: tableId,
+                settings: data
+            }
+        }
+    } catch {
+        return {
+            redirect: {
+                destination: "/error"
+            }
+        }
+    }
+}
+
+// interface Styles {
+//     '--bg-color': string;
+//     '--primary-color': string;
+//     '--text-color': string;
+// }
+
+
+const Index: NextPage<any> = ({restaurantId, tableId, settings}: {restaurantId: string, tableId: string, settings: any}) => {
     const router = useRouter();
-    const {tableId, restaurantId} = router.query;
+
+
+    const addStylesToSessionStorage = () => {
+        //Save styles on first QR code scan to local storage
+        localStorage.setItem('styles', JSON.stringify({
+            '--bg-color': settings.backgroundColour,
+            '--primary-color': settings.primaryColour,
+            '--text-color': settings.textColour
+        }));
+
+        //To retrieve them in other pages use the following code:
+        // Retrieve styles from localStorage
+        // const savedStyles = JSON.parse(localStorage.getItem('styles')!) as Styles;
+
+        // Apply the saved styles
+        // Object.entries(savedStyles).forEach(([property, value]) => {
+        //    document.documentElement.style.setProperty(property, value);
+        // });
+
+
+    }
 
     const addItemsToSessionStorage = () => {
         sessionStorage.setItem("tableId", tableId as string)
         sessionStorage.setItem("restaurantId", restaurantId as string)
     }
+
     useEffect(() => {
         if (!router.isReady) return;
-        addItemsToSessionStorage()
-        router.push('/' + restaurantId)
+        addItemsToSessionStorage();
+        addStylesToSessionStorage();
+        // router.push('/' + restaurantId)
     })
 
     return (
-        <div>
-
-        </div>
+      <></>
     )
 }
 export default Index
